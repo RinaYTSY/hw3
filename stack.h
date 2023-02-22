941364ddbd9b5ab65e4bd5c44cb6b0ad39505d9a
Rina Yang <yangsabr@usc.edu>
2023-02-22 02:36:46 +0000

    finished

--- stack.h
+++ stack.h
@@ -28,36 +28,36 @@ Stack<T>::~Stack(){
 
 template <typename T>
 bool Stack<T>::empty() const{
-    return this->empty();
+    return std::vector<T>::empty();
 }
 
 template <typename T>
 size_t Stack<T>::size() const{
-    return this->size();
+    return std::vector<T>::size();
 }
 
 template <typename T>
 void Stack<T>::push(const T& item){
-    this->push_back(item);
+    std::vector<T>::push_back(item);
     return;
 
 }
 
 template <typename T>
 void Stack<T>::pop(){
-    if(this->empty()){
-        std::underflow_error("empty stack");
+    if(std::vector<T>::empty()){
+        std::underflow_error("Empty stack");
     }
-    this->pop_back();
+    std::vector<T>::pop_back();
     return;
 }
 
 template <typename T>
 const T& Stack<T>::top() const{
-    if(this->empty()){
-        std::underflow_error("empty stack");
+    if(std::vector<T>::empty()){
+        std::underflow_error("Empty stack");
     }
-    return this->back();
+    return std::vector<T>::back();
 
 }
 
